89ac3b0d8715fa8cbd355218721c08ee1479abe4
Brad Newman <newmanbradm@gmail.com>
2018-05-20 16:22:22 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   hash = {}
   data.each do |attribute, attribute_hash|
     attribute_hash.each do |key, array|
-      arrary.each do |name|
+      array.each do |name|
         hash[name] ||= {}
         hash[name][attribute] ||= []
         hash[name][attribute] << key.to_s
